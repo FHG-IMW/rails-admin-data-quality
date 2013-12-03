78eadd665e1cc78869fa1969f8e4e87b36d029c6
Max Kießling <max.kiessling@moez.fraunhofer.de>
2013-12-03 13:28:34 +0100

    rescue if recovery of object history fails

--- app/helpers/rails_admin/data_quality_view_helper.rb
+++ app/helpers/rails_admin/data_quality_view_helper.rb
@@ -4,35 +4,35 @@ module RailsAdmin
 
       scores = [object.quality_score]
 
-      if version=object.previous_version
+      version = (object.previous_version rescue nil)
 
+      if version
         scores << version.quality_score if version.quality_score
 
         n.times do
-          if version=version.previous_version
-            scores << version.quality_score if version.quality_score
-          else
+          begin
+            version=version.previous_version
+            if version
+              scores << version.quality_score
+            else
+              break
+            end
+          rescue
             break
           end
         end
-
       end
 
       scores.reverse.join(",")
     end
 
     def avg_quality_score(objects)
+
       objects.sum { |object| object.quality_score || 0}/objects.count
     end
 
     def max_quality_score(objects)
-      maximum=0
-      objects.each do |object|
-        unless object.quality_score.blank?
-          maximum=object.quality_score if object.quality_score > maximum
-        end
-      end
-      maximum
+      objects.map(&:quality_score).max
     end
 
     def object_name_for(object = @object, abstract_model = @abstract_model)
